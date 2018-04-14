46b97d646a17eb22a47eee5bd2964e6dd7751c56
jacobsilver2 <jacobsilver2@mac.com>
2018-04-14 04:34:33 -0400

    successfully created new show form, with form partial

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,2 +1,35 @@
 class ShowsController < ApplicationController
+  before_action :find_show, only:[:show, :edit, :update, :destroy]
+
+  def show  
+  end
+
+  def new
+    @show = Show.new
+  end
+
+  def create
+    @show = Show.new(show_params)
+    respond_to do |format|
+      if @show.save
+        format.html { redirect_to @show, notice: 'Show was successfully created.' }
+      else
+        format.html {render :new }
+      end
+    end
+  end
+
+  def update
+  end
+
+  private
+  def find_show
+    @show = Show.find(params[:id])
+  end
+
+  def show_params
+    params.require(:show).permit(:date, :start_time, :confirmed, :user_id, :act_id, :notes)
+  end
+
+
 end
---
+++ todo.md
@@ -0,0 +1,10 @@
+TODO
+
+build out basic CRUD methods for show and act
+
+
+create a home view for a logged in user, give them 2 buttons: one to create a show, and another to add an act to the database.
+
+build forms for creating new shows and new acts.
+
+
