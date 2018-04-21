8263604615471929c14df877bb6490d03d3bd547
jacobsilver2 <jacobsilver2@mac.com>
2018-04-21 08:09:54 -0400

    SUCCESS

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,20 +2,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook]
          
-  validates_presence_of :name
+  # validates_presence_of :name
   has_many :shows
   
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
+      user.provider = auth.provider
+      user.uid = auth.uid
       user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      
-      # If you are using confirmable and the provider(s) you use validate emails, 
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  root 'shows#index'
+  # root 'shows#index'
   get '/shows/:id/destroy', to: 'shows#destroy'
   get '/acts/:id/destroy', to: 'shows#destroy'
   
-  devise_for :users, :controllers => { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
-
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  authenticated :user do
+		root 'shows#index', as: 'authenticated_root'
+	end
+	devise_scope :user do
+		root 'devise/sessions#new'
+	end
   resources :shows, :only => [:index, :show, :edit, :update, :destroy, :create]
   resources :acts, :only => [:index, :show, :edit, :update, :destroy, :create]
   
