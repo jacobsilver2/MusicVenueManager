e729c7f64d0594f54600126a36b3411f7bf2b59c
jacobsilver2 <jacobsilver2@mac.com>
2018-06-01 11:16:33 -0400

    created viewShows js. stubbed out viewShow.

--- app/assets/javascripts/musicvenuemanager.js
+++ app/assets/javascripts/musicvenuemanager.js
@@ -16,6 +16,20 @@ function createShow() {
 }
 
 function viewShows() {
-    alert("you clicked view shows")
+    $('#shows').empty();
+    $.get("/shows", function(shows){
+        if (shows.length) {
+            shows.forEach(function(show){
+                let showHTML = `<button id="showid-${show.id}" class="btn btn-secondary">${show.date}</button><br>`;
+                $('#shows').append(showHTML);
+                $(`#showid-${show.id}`).on('click', function(){
+                    viewShow(show.id);
+                });
+            })
+        }
+    })
 }
 
+function viewShow(showId) {
+    alert("you want to view a show")
+}
